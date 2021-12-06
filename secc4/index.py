01118a7ed28d81727561b0d44093604898eb8f5c
lecav <lecav30@outlook.com>
2021-12-06 18:34:16 -0500

    Introducción, conceptos básicos

---
+++ secc4/index.py
@@ -0,0 +1,47 @@
+# Identacion #
+
+if 5 > 3:
+    print('Es mayor a 3')
+
+# A diferencia de lenguajes como C++, Python utiliza mucho la identación, por lo que 
+# es una parte fundamental al momento de programar
+
+# Comentarios #
+
+# Son algo básico y necesario en cualquier lenguaje de programación, como se puede apreciar
+# los comentarios en python se realizan tipeando un # antes de lo que se desea comentar
+
+# Variables #
+
+# Python utiliza variables generales, por así decirlo, ya que no es necesario declarar el
+# tipo de variable antes del nombre de la variable. Además, el nombre de las varibles es
+# completamente arbitrario. Cabe resaltar, que es recomendable que las variables sean 
+# representativas
+
+x = 5
+y = 'oink happy'
+
+PI = 3.1416
+# Dato: Normalmente las variables declaradas en mayúsculas son tipos de datos constantes e inalterables
+
+print(x, y)
+
+# Definición de múltiples variables #
+
+a, b, c, d, e = 'Pata', 'Peta', 'Pita', 'Pota', 'Maria'
+
+print(a, b, c, d, e)
+
+v1 = v2 = v3 = 'F'
+
+print(v1, v2, v3)
+
+# Concatenación #
+
+inicio = 'Hola '
+fin = 'Mundo'
+
+print(inicio + fin)
+
+# Como se aprecia al compilar el programa, la , en el print() da un espacio entre las variabels,
+# en cambio el + "junta" a ambas variables
