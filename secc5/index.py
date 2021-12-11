74a57a34ea9c5e1adf8cb3365b964c948575dc0c
lecav <lecav30@outlook.com>
2021-12-10 19:38:58 -0500

    Strings y métodos de listas, tuplas y diccionarios

--- secc5/index.py
+++ secc5/index.py
@@ -14,14 +14,17 @@ complejo = 1j # es necesario utilizar una j al final de una variable de numero c
 # Listas #
 
 #lista = [1, 2, 3] 
-lista = ['Hola', 'Mundo', 'oink happy']
+lista = ['Hola', 'Mundo', 'Chanchito feliz']
 
 # Al imprimir una lista vacia solo se muestran los corchetes: []
 
 # Para copiar una lista a otra:
 lista2 = lista.copy()
+
 # Para agregar un elemento a la lista:
-lista.append(4)
+#lista.append(4)
+lista.append('Chanchito triste')
+
 # Para eliminar todos los elementos de la lista:
 #lista.clear()
 
@@ -29,13 +32,107 @@ lista.append(4)
 
 # Para contar la cantidad de repeticiones de un elemento de la lista:
 #print(lista2.count(3))
+
 # Para contar los elementos de la lista:
 #print(len(lista2))
 largo_lista = len(lista)
 #print(largo_lista)
 
 # Para acceder a los elementos de la lista:
-print(lista[0])
+# print(lista[0])
 # Siempre se utilizan indices y no la posición del elemento
+
+# Para borrar el último elemento de la lista:
+# lista.pop()
+# print(lista)
+
+# Para eliminar un elemento en concreto
+# lista.remove('Hola')
+# print(lista)
+
+# Para revertir el orden de la lista:
+# lista.reverse()
+# print(lista)
+
+# Para ordenar la lista en orden ascendente:
+lista.sort()
+# print(lista)
+
+# Tuplas #
+
+# A diferencia de las listas, no puedes modificar a las tuplas
+
+tupla = ('Hola','mundo','somos','tupla')
+# print(tupla)
+
+# Para contar la cantidad de repeticiones de un elemento de la tupla:
+# print(tupla.count('mundo'))
+
+# # Para devolver el índice del elemento:
+# print(tupla.index('mundo'))
+
+# Como las tuplas no se pueden modificar se deben de castear a una lista
+# lista_de_tupla = list(tupla)
+# lista_de_tupla.append('Chanchito')
+# print(lista_de_tupla)
+
+# Rangos #
+
+rango = range(6)
+# print(rango)
+
+# Diccionarios #
+
+diccionario =  {
+        "nombre" : "Mishifu",
+        "raza" : "Persa",
+        "edad" : 5
+        }
+
+# print(diccionario)
+# Para acceder a un valor se debe digitar la llave entre corchetes:
+# print(diccionario['nombre'])
+# Sin embargo un método para acceder a los valores es:
+# print(diccionario.get('nombre'))
+
+# Para modificar algún valor:
+diccionario['nombre'] = 'MishiGod'
+# print(diccionario)
+
+# Para saber la cantidad de elementos:
+# print(len(diccionario))
+
+# Para agregar una llave con su valor:
+diccionario['ronronea'] = 'Si'
+# print(diccionario)
+
+# Para eliminar una propiedad(llave y valor):
+# diccionario.pop('raza')
+# o 
+# del diccionario['ronronea']
+
+# Para eliminar la última propiedad ingresada al diccionario:
+# diccionario.popitem()
+
+# Para copiar el diccionario:
+# copia_gato = diccionario.copy()
+# Otra forma:
+copia_gato = dict(diccionario)
+del diccionario['ronronea']
+
+# Para limpiar el diccionario:
+diccionario.clear()
+
+print(diccionario, copia_gato)
+
+
+
+
+
+
+
+
+
+
 
 
